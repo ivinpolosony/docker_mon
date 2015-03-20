505ffcac3f216f2d3c2e035b7ff0bd493386fae8
ivinpolosony@gmail.com <ivinpolosony@gmail.com>
2015-03-20 19:44:05 +0000

    Twitter auth added

---
+++ dockermon/dockermon/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'dockermon.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+   url(r'^$', 'dockermon.views.home', name='home'),
+   url('', include('social.apps.django_app.urls', namespace='social')),
+   url('', include('django.contrib.auth.urls', namespace='auth')),
+    url(r'^admin/', include(admin.site.urls)),
+)
